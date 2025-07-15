034abf0060e9f4756b8f1820805609332b5f46be
Aditya Giri <74224708+adityagiri3600@users.noreply.github.com>
2025-07-15 19:51:36 +0530

    add: pdf support

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ from getpass import getpass
 from google import genai
 from cryptography.fernet import InvalidToken
 
+# pip install pymupdf
+import fitz  # PyMuPDF
+def pdf_to_text(pdf_path):
+    pdf_document = fitz.open(pdf_path)
+    
+    text = ''
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_num)
+        text += page.get_text()
+
+    return text
+
 password = getpass("password: ").encode()
 key = base64.urlsafe_b64encode(hashlib.sha256(password).digest())
 
@@ -18,9 +30,22 @@ except InvalidToken:
     print("Incorrect password")
     exit(1)
 
-client = genai.Client(api_key=api_key)
+text = ''
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents=input("q: ")
-)
-print(response.text)
+if input("Do you want to attach a PDF? (y/N): ").strip().lower() == 'y':
+    pdf_path = input("PDF path: ")
+    text = pdf_to_text(pdf_path)
+    contents = "Attached pdf document:\n" + "-------------PDF BEGIN-------------\n" + text + "\n-------------PDF END-------------\n" + "user prompt: " + input("q: ")
+else:
+    contents = input("q: ")
+    
+client = genai.Client(api_key=api_key)
+while True:
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents=contents
+    )
+    print(response.text)
+    user_input = input("q: ")
+    if user_input.lower() in ['exit', 'quit']:
+        break
+    contents += "\nagent: " + response.text + "\nuser: " + user_input + "\nagent: "
